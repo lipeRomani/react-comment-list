ea36d77d9eade50f37e54d5458a01462647a4abf
Felipe Romani <felipearomani@gmail.com>
2018-08-06 23:25:22 -0300

    Adicionado middleware de validacao da store do redux

--- src/Root.js
+++ src/Root.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-// import reduxPromise from 'redux-promise';
 import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 
 import reducers from 'reducers';
 
@@ -10,7 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(reducers, initialState, composeEnhancers(
-    applyMiddleware(async)
+    applyMiddleware(async, stateValidator)
   )); 
   return (
     <Provider store={store}>
